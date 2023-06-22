67eed1a24d4922739f8fbcc399424955bdc0c64f
aaradhyakul <aaradhyakul@gmail.com>
2023-06-22 15:22:43 +0530

    navigator object

---
+++ index.js
@@ -0,0 +1,19 @@
+const lat = document.getElementById("latitude");
+const long = document.getElementById("longitude");
+const alt = document.getElementById("altitude");
+const speed = document.getElementById("speed");
+
+const geoCheck = ()=>{if("geolocation" in navigator){
+	navigator.geolocation.getCurrentPosition((pos)=>{
+		lat.textContent = pos.coords.latitude;
+		long.textContent = pos.coords.longitude;
+		alt.textContent = pos.coords.altitude===null?"No altitude system support" : pos.coords.altitude;
+		speed.textContent = pos.coords.speed===null?"No speed system support" : pos.coords.speed;
+	});
+}
+else{
+	console.log("use IP approximation");
+}
+
+}
+geoCheck();
